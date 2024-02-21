7d82d9c48bf03729b8e3d9b6865e74fe00bb39dc
unknown <Sena@MEDAPRCSGFSD729.FormacionSena.RED>
2024-02-21 14:47:42 -0500

    form1

---
+++ lib/formbasico.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:statefull/main.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter code SAmplers';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: _title,
+        home: Scaffold(
+          appBar: AppBar(title: Text(_title)),
+          body: const MyStatefulWidget(),
+        ));
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  String? value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formkey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Enter your email',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your email';
+              }
+              // Expresión regular para validar un email
+              String pattern =
+                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\\.,;:\s@\"]+\.)+[^<>()[\]\\.,;:\s@\"]{2,})$';
+              RegExp regex = RegExp(pattern);
+              if (!regex.hasMatch(value)) {
+                return 'Enter a valid email address';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formkey.currentState!.validate()) {}
+              },
+              child: const Text('Submit'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
